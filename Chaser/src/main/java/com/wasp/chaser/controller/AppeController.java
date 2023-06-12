e51467d80edde383e989f1f2bcad8695b917dc44
SMHRD <SMHRD@220.71.97.208>
2023-06-12 15:40:03 +0900

    김용훈, 유종국 - 페이지 수정

--- Chaser/src/main/java/com/wasp/chaser/controller/AppeController.java
+++ Chaser/src/main/java/com/wasp/chaser/controller/AppeController.java
@@ -1,5 +1,4 @@
 package com.wasp.chaser.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
