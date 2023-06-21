5fb221e47515092f55c0941c46f772a0951d6a2a
smhrd <smhrd@220.80.165.49>
2023-06-21 12:28:17 +0900

    심우석 - apiCon 수정

--- Chaser/src/main/java/com/wasp/chaser/controller/ApiController.java
+++ Chaser/src/main/java/com/wasp/chaser/controller/ApiController.java
@@ -84,7 +84,7 @@ public class ApiController {
 				// 데이터 담을 객체 생성
 				RestTemplate restTemplate = new RestTemplate();
 
-				String url = "http://localhost:9091/modeling";
+				String url = "http://localhost:9091/";
 
 				// 객체 Header
 				HttpHeaders httpHeaders = new HttpHeaders();
